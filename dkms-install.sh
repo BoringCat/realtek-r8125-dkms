8698fe49926bd446b139057c09709dddc9c2c6a7
boringcat <c654477757@gmail.com>
2024-11-10 10:21:01 +0800

    support build after kernel updated

--- dkms-install.sh
+++ dkms-install.sh
@@ -7,18 +7,33 @@ else
   echo "About to run dkms install steps..."
 fi
 
+function getKernelVersions() {
+  find /lib/modules     -maxdepth 2 -name kernel -type d | cut -d/ -f4
+  find /usr/lib/modules -maxdepth 2 -name kernel -type d | cut -d/ -f5
+}
+
+function buildForExistsKernel() {
+  for _kver in `getKernelVersions | sort | uniq`
+  do
+    dkms build -m ${DRV_NAME} -v ${DRV_VERSION} -k ${_kver}
+    dkms install -m ${DRV_NAME} -v ${DRV_VERSION} -k ${_kver}
+  done
+}
+
 DRV_DIR="$(pwd)"
 DRV_NAME=r8125
 DRV_VERSION=9.014.01
+DKMS_DIR=/var/lib/dkms
+
+set -e
 
 rsync -av --delete-after --delete-excluded \
   --exclude=.git/ --exclude=debian/ --exclude=.gitignore --exclude='*.sh' \
   --exclude-from=.gitignore \
   ${DRV_DIR}/ /usr/src/${DRV_NAME}-${DRV_VERSION}/
 
-dkms add -m ${DRV_NAME} -v ${DRV_VERSION}
-dkms build -m ${DRV_NAME} -v ${DRV_VERSION}
-dkms install -m ${DRV_NAME} -v ${DRV_VERSION}
+[ ! -d "${DKMS_DIR}/${DRV_NAME}/${DRV_VERSION}" ] && dkms add -m ${DRV_NAME} -v ${DRV_VERSION}
+buildForExistsKernel
 RESULT=$?
 
 echo "Finished running dkms install steps."
