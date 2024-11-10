87c1da22a920398d28cf15ebf0f9aea514f96c13
boringcat <c654477757@gmail.com>
2024-11-10 10:06:11 +0800

    update 9.014.01 from realtek

--- dkms-install.sh
+++ dkms-install.sh
@@ -9,7 +9,7 @@ fi
 
 DRV_DIR="$(pwd)"
 DRV_NAME=r8125
-DRV_VERSION=9.013.02
+DRV_VERSION=9.014.01
 
 rsync -av --delete-after --delete-excluded \
   --exclude=.git/ --exclude=debian/ --exclude=.gitignore --exclude='*.sh' \
